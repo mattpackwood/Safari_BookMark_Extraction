bbda06543b66197e8c6264a71235472a0ddb48d0
Matt Packwood <mattpackwood@gmail.com>
2022-07-20 16:35:48 -0400

    Create get-safari-bookmarks.sh

---
+++ get-safari-bookmarks.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# get Safari Bookmarks
+bm="$HOME/Library/Safari/Bookmarks.plist"
+grep -A1 -E '(>URLString<|>title<)' $bm |
+grep -v -E '(>URLString|>title|^--)' |
+cut -d\> -f2 | cut -d\< -f1 |
+while read theTitle
+do
+read theUrl
+echo "<!-- $theTitle --><a href=\"$theUrl\">$theTitle</a><br />"
+done |
+sort -f |
+uniq
+exit 0
